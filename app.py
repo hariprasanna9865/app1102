22549543cd58f93b7a4af6e8341011430ac0e24a
Hari Prasanna M <141819496+hariprasanna9865@users.noreply.github.com>
2026-05-02 11:58:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
 import joblib
 
